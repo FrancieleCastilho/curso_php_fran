9081a1ce1d4ec3656a46894e109b3329f47ae09b
Franciele <francielecasteves@gmail.com>
2018-11-03 14:13:16 -0300

    Super Classe Conta

--- Conta.php
+++ Conta.php
@@ -3,6 +3,7 @@
 class Conta{
     private $numero;
     private $cliente;
+    protected $saldo;
     
 
     public function setNumero($numero){
@@ -17,6 +18,19 @@ class Conta{
     }
     public function getCliente(){
         return $this->cliente;
+    }
+
+    public function setSaldo($valor){
+        $this->saldo = $valor;
+    }
+    public function getSaldo(){
+        return $this->saldo;
+    }
+    public function depositar($valor){
+        $this->saldo += $valor;
+    }
+    public function sacar($valor){
+        $this->saldo -= $valor;
     }
 
 }
--- Exibir.php
+++ Exibir.php
@@ -1,11 +1,26 @@
 <?php
 require_once("Conta.php");
+require_once("Funcoes.php");
+require_once("ContaCorrente.php");
+require_once("ContaPoupanca.php");
 
 $conta = new Conta;
-
 $conta->setNumero("1234");
 $conta->setCliente("João");
+$conta->setSaldo(200);
+imprimir("Cliente: ". $conta->getCliente() . " Conta nº " . $conta->getNumero());
+imprimir("Saldo: ". $conta->getSaldo());
 
-echo $conta->getNumero();
-echo "</br>";
-echo $conta->getCliente();
+$conta->depositar(100);
+imprimir("Saldo + Deposito: ". $conta->getSaldo());
+
+$conta->sacar(50);
+imprimir("Saldo - Saque: ". $conta->getSaldo());
+
+$credito = new ContaCorrente;
+$credito->credito($conta->getSaldo());
+imprimir("Credito pessoal (Saldo*10): ". $credito->getSaldo());
+
+$poupanca = new ContaPoupanca;
+$poupanca->poupanca($conta->getSaldo());
+imprimir("O Saldo total (Saldo+Poupança) é de : ". $poupanca->getSaldo());
