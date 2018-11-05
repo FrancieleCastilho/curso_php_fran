41d2ba2421bba1776eba9b700ef1c22082fde0e8
Franciele <francielecasteves@gmail.com>
2018-11-05 19:13:11 -0200

    utilizando variaveis para salvar dados

---
+++ Variaveis.php
@@ -0,0 +1,9 @@
+<?php
+
+$nome = "Franciele";
+$sobrenome = "Castilho";
+$endereço = "Cachoeira Paulista";
+$idade = 25; 
+$telefone = 123456789;
+
+echo "Olá meu nome é ".$nome." ".$sobrenome." tenho ".$idade. " anos, moro em ".$endereço. " e meu telefone é: ".$telefone."!";
