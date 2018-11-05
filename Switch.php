8a48904283f08d5ed16968993dc2ebd7ea429e72
Franciele <francielecasteves@gmail.com>
2018-11-05 20:43:01 -0200

    SWITCH

---
+++ Switch.php
@@ -0,0 +1,30 @@
+<?php
+
+echo "(1)Frango com batata e salada <br>";
+echo "(2)Frango ao molho madeira<br>";
+echo "(3)Frango parmegiana<br>";
+echo "(4)Frango ao molho xadres<br>";
+echo "(5)Frango em tiras<br>";
+echo "(6)Frango ao molho madeira, batata e salada<br><br>";
+
+$opcao = 4;
+switch ($opcao){
+    case(1):
+    echo "Você escolheu: Frango com batata e salada";
+    break;
+    case(2):
+    echo "Você escolheu: Frango ao molho madeira";
+    break;
+    case(3):
+    echo "Você escolheu: Frango parmegiana";
+    break;
+    case(4):
+    echo "Você escolheu: Frango ao molho xadres";
+    break;
+    case(5):
+    echo "Você escolheu: Frango em tiras";
+    break;
+    case(6):
+    echo "Você escolheu: Frango ao molho madeira, batata e salada";
+    break;
+}
