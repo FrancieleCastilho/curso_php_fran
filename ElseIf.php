6ee1c7c3475f1587f1fcb8973faf7e932ba80d00
Franciele <francielecasteves@gmail.com>
2018-11-05 19:48:10 -0200

    Nova sintaxe IF

--- ElseIf.php
+++ ElseIf.php
@@ -1,15 +1,15 @@
 
 <?php
-$salario = 2200;
+$salario = 1600;
 
-if ($salario <= 1000){
+if ($salario <= 1000):
     echo "Você não paga imposto";
-}elseif($salario  <= 1500){
+elseif($salario  <= 1500):
     echo "Você paga 10% de imposto";
-}elseif($salario <=2000){
+elseif($salario <=2000):
     echo "Você paga 15% de imposto";
-}else{
+else:
     echo "Você paga 20% de imposto";
-}
+endif;
 
 
