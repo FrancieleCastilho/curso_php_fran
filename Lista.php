2f6cd64586b900d136f2c5ee0e0dbf732571e73b
Franciele <francielecasteves@gmail.com>
2018-11-08 23:32:08 -0200

    Lista de Funcionarios

---
+++ Lista.php
@@ -0,0 +1,12 @@
+<?php
+$funcionarios = array("Vitor", "Pedro", "Gabriel", "Lucas", "Reinaldo", "Ana", "Jessika", "Franciele", "Flaviane", "Karina");
+echo $funcionarios[0] . "<br>";
+echo $funcionarios[1] . "<br>";
+echo $funcionarios[2] . "<br>";
+echo $funcionarios[3] . "<br>";
+echo $funcionarios[4] . "<br>";
+echo $funcionarios[5] . "<br>";
+echo $funcionarios[6] . "<br>";
+echo $funcionarios[7] . "<br>";
+echo $funcionarios[8] . "<br>";
+echo $funcionarios[9] . "<br>";
