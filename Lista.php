2875715e1010b73632bca879930a6ace4370d536
Franciele <francielecasteves@gmail.com>
2018-11-09 00:24:39 -0200

    Array DO WHILE

--- Lista.php
+++ Lista.php
@@ -1,12 +1,8 @@
 <?php
 $funcionarios = array("Vitor", "Pedro", "Gabriel", "Lucas", "Reinaldo", "Ana", "Jessika", "Franciele", "Flaviane", "Karina");
-echo $funcionarios[0] . "<br>";
-echo $funcionarios[1] . "<br>";
-echo $funcionarios[2] . "<br>";
-echo $funcionarios[3] . "<br>";
-echo $funcionarios[4] . "<br>";
-echo $funcionarios[5] . "<br>";
-echo $funcionarios[6] . "<br>";
-echo $funcionarios[7] . "<br>";
-echo $funcionarios[8] . "<br>";
-echo $funcionarios[9] . "<br>";
+$contador=0;
+
+do{
+    echo $funcionarios[$contador] . "<br>";
+    $contador ++;
+}while($contador < count($funcionarios));
