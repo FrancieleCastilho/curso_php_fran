7c1e1dcb971f41a76fecaad3d2216572317e07c8
Franciele <francielecasteves@gmail.com>
2018-11-09 00:42:08 -0200

    Array FOREACH

---
+++ Campeonato.php
@@ -0,0 +1,26 @@
+<?php
+
+$times = array("Palmeiras",
+"Internacional",
+"Internacional",
+"São Paulo",
+"Grêmio",
+"Atlético-MG",
+"Santos",
+"Cruzeiro",
+"Atlético-PR",
+"Atlético-PR",
+"Bahia",
+"Corinthians",
+"Vasco",
+"Botafogo",
+"Ceará",
+"Sport",
+"Vitória",
+"América-MG",
+"Chapecoense",
+"Paraná");
+
+foreach($times as $time){
+    echo $time."<br>";
+}
