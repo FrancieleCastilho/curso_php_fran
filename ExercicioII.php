c006c54d503294f2e14ec89ce6d9a735ef7d2026
Franciele <francielecasteves@gmail.com>
2018-11-08 22:19:33 -0200

    EXERCICIOII

--- ExercicioII.php
+++ ExercicioII.php
@@ -1,5 +1,5 @@
 <?php
- $numero = 20;
+ $numero = 3;
 
  if((($numero % 10) == 0) && (($numero % 5) == 0) && (($numero % 2) == 0)):
     echo $numero . " é divisivel por 10 por 5 e por 2";
@@ -7,5 +7,6 @@
         echo $numero . " é divisivel por 5";
     elseif(($numero % 2 ) == 0):
         echo $numero . " é divisivel por 2";
-        
+    else:
+        echo "Não é divisivel por 10, 5 ou 2";
     endif;
