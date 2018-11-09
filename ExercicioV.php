dff610ba5a550100c78ff0e4789133be3e7aa45d
Franciele <francielecasteves@gmail.com>
2018-11-08 23:03:14 -0200

    EXERCICIOV

---
+++ ExercicioV.php
@@ -0,0 +1,28 @@
+<?php
+
+$valor=3;
+$resultado;
+ if($valor>0){
+     $resultado = $valor * 1;
+     echo $valor. " x 1 = " .$resultado . "<br>";
+     $resultado = $valor * 2;
+     echo $valor. " x 2 = " .$resultado . "<br>";
+     $resultado = $valor * 3;
+     echo $valor. " x 3 = " .$resultado . "<br>";
+     $resultado = $valor * 4;
+     echo $valor. " x 4 = " .$resultado . "<br>";
+     $resultado = $valor * 5;
+     echo $valor. " x 5 = " .$resultado . "<br>";
+     $resultado = $valor * 6;
+     echo $valor. " x 6 = " .$resultado . "<br>";
+     $resultado = $valor * 7;
+     echo $valor. " x 7 = " .$resultado . "<br>";
+     $resultado = $valor * 8;
+     echo $valor. " x 8 = " .$resultado . "<br>";
+     $resultado = $valor * 9;
+     echo $valor. " x 9 = " .$resultado . "<br>";
+     $resultado = $valor * 10;
+     echo $valor. " x 10 = " .$resultado . "<br>";
+ }else{
+     echo "O valor informado não é valido";
+ }
