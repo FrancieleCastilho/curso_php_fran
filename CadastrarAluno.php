984db2f966f34607ed9ba5a1c9af132fce378a0b
Franciele <francielecasteves@gmail.com>
2018-11-10 20:28:08 -0200

    Formulario de Cadastro

---
+++ CadastrarAluno.php
@@ -0,0 +1,4 @@
+<?php
+ echo "Dados dos Alunos <br><br>";
+ echo "Nome: ". $_GET["nome"]."<br>";
+ echo "Turma: ". $_GET["turma"]."<br>";
---
+++ Formulario.php
@@ -0,0 +1,7 @@
+<form action="CadastrarAluno.php" method="GET">
+    <label> Nome </label><br>
+    <input type="text" name="nome"/><br>
+    <label> Turma </label><br>
+    <input type="text" name="turma"/><br>
+    <input type="submit" value ="Salvar"/><br>
+</form>
