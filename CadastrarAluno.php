3cd918aadac99674f1dbdb3b1d035186f7b278e7
Franciele <francielecasteves@gmail.com>
2018-11-10 21:33:35 -0200

    Formulario com RadioButon

--- CadastrarAluno.php
+++ CadastrarAluno.php
@@ -1,4 +1,14 @@
 <?php
  echo "Dados dos Alunos <br><br>";
- echo "Nome: ". $_POST["nome"]."<br>";
+ echo "Nome: ". $_POST["nome"]." ".$_POST["sobrenome"]."<br>";
  echo "Turma: ". $_POST["turma"]."<br>";
+ echo "Período: ";
+ if ($_POST["periodo"]=="Manha"){
+    echo "Manhã";
+ }elseif($_POST["periodo"]=="Tarde"){
+    echo "Tarde";
+ }elseif($_POST["periodo"]=="Noite"){
+    echo "Noite";
+ }else{
+     echo "Usuário não informou o período em que estuda";
+ }
--- Formulario.php
+++ Formulario.php
@@ -1,7 +1,13 @@
 <form action="CadastrarAluno.php" method="POST">
     <label> Nome </label><br>
     <input type="text" name="nome"/><br>
+    <label> Sobrenome </label><br>
+    <input type="text" name="sobrenome"/><br>
     <label> Turma </label><br>
     <input type="text" name="turma"/><br>
+    <label> período</label><br>
+    <input type="radio" name="periodo" value="Manha"/> Manhã
+    <input type="radio" name="periodo" value="Tarde"/> Tarde
+    <input type="radio" name="periodo" value="Noite"/> Noite <br>  
     <input type="submit" value ="Salvar"/><br>
 </form>
