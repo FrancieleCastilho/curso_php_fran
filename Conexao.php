16aa7510ebfb636010098fa6b7042c252f2a82df
Franciele <francielecasteves@gmail.com>
2018-11-11 00:20:40 -0200

    Acessando o banco

--- Conexao.php
+++ Conexao.php
@@ -3,27 +3,21 @@
 $servidor = "cursophp_db_1";
 $usuario = "root";
 $senha = "phprs";
+$banco = "curso_php";
 
-$conn = new  mysqli($servidor, $usuario, $senha);
+$conn = new  mysqli($servidor, $usuario, $senha, $banco);
 
 if ($conn->connect_error){
     die ("Erro ao conectar ao banco de dados". $conn->connect_error);
 }
 
-echo "Conexao efetuada com sucesso";
+$sql = "SELECT * FROM `usuarios` ";
+$result = $conn->query($sql);
 
-/* Erro ao errar o Nome do Banco 
+if ($result->num_rows > 0){
+    echo "o numero de registro é: ".  $result->num_rows;
+}else{
+    echo "não existe nenhum registro";
+}
+$conn->close();
 
-Warning: mysqli::__construct(): php_network_getaddresses: getaddrinfo failed: Name or service not known in /var/www/html/iniciandoPHP/Index.php on line 7
-
-Warning: mysqli::__construct(): (HY000/2002): php_network_getaddresses: getaddrinfo failed: Name or service not known in /var/www/html/iniciandoPHP/Index.php on line 7
-Erro ao conectar ao banco de dadosphp_network_getaddresses: getaddrinfo failed: Name or service not known
-
-Erro ao errar o Usuario
-Warning: mysqli::__construct(): (HY000/1045): Access denied for user 'root1'@'172.17.0.4' (using password: YES) in /var/www/html/iniciandoPHP/Index.php on line 7
-Erro ao conectar ao banco de dadosAccess denied for user 'root1'@'172.17.0.4' (using password: YES)
-
-Erro ao errar a senha
-Warning: mysqli::__construct(): (HY000/1045): Access denied for user 'root'@'172.17.0.4' (using password: YES) in /var/www/html/iniciandoPHP/Index.php on line 7
-Erro ao conectar ao banco de dadosAccess denied for user 'root'@'172.17.0.4' (using password: YES)
-*/
