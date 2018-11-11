42d291d9d8c8fb864d7acccbbe8813392d52a641
Franciele <francielecasteves@gmail.com>
2018-11-11 00:34:04 -0200

    Listando os dados da tabela

--- Conexao.php
+++ Conexao.php
@@ -16,6 +16,15 @@ $result = $conn->query($sql);
 
 if ($result->num_rows > 0){
     echo "o numero de registro é: ".  $result->num_rows;
+    echo "<br><br>";
+
+    while($row =  $result->fetch_assoc()){
+        echo "ID: " .$row["id"]."<br>";
+        echo "Email: " .$row["email"]."<br>";
+        echo "Senha: " .$row["senha"]."<br>";
+        echo "<hr>";
+        
+    }
 }else{
     echo "não existe nenhum registro";
 }
